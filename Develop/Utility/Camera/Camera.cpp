e89b58425a9f67429af53706ce83e666b35ca6c2
higa-shunnosuke <138739372+higa-shunnosuke@users.noreply.github.com>
2025-01-27 14:33:41 +0900

    Cameraクラス
    ・Singletonクラスを継承
    ・コメントを追加

--- Develop/Utility/Camera/Camera.cpp
+++ Develop/Utility/Camera/Camera.cpp
@@ -1,6 +1,7 @@
 #include "Camera.h"
 #include "../ProjectConfig.h"
 
+// コンストラクタ
 Camera::Camera():
 	player(nullptr),
 	location()
@@ -8,17 +9,21 @@ Camera::Camera():
 
 }
 
+// デストラクタ
 Camera::~Camera()
 {
 
 }
 
+// 初期化処理
 void Camera::Initialize()
 {
+	// カメラの初期座標を設定
 	this->location.x = D_WIN_MAX_X / 2;
 	this->location.y = D_WIN_MAX_Y / 2;
 }
 
+// 更新処理
 void Camera::Update()
 {
 	// プレイヤーを追う処理
@@ -28,25 +33,29 @@ void Camera::Update()
 	}
 	else
 	{
+		// DEBUGフラグ = TRUE   : プレイヤーが戻るとカメラも追従する
+		// DEBUGフラグ = FARUSE : プレイヤーが戻るとカメラは追従しない
 #ifdef DEBUG
 		location.x = player->GetLocation().x;
 #endif
 	}
 
+	//ステージ外にいかないようにする処理
 	float right;
-	right = 6400;
-	 //ステージ外にいかないようにする処理
+	right = STAGE_SIZE_X * BOX_SIZE - D_WIN_MAX_X / 2;
 	if (location.x > right)
 	{
 		location.x = right;
 	}
 }
 
+// カメラ情報取得処理
 Vector2D Camera::GetCameraPos()
 {
 	return this->location;
 }
 
+// プレイヤー情報設定処理
 void Camera::SetPlayer(Player* player)
 {
 	this->player = player;
--- Develop/Utility/Camera/Camera.h
+++ Develop/Utility/Camera/Camera.h
@@ -1,21 +1,41 @@
 #pragma once
 
 #include "../../Objects/Character/Player/Player.h"
+#include "../Singleton.h"
 
-class Camera
+class Camera : public Singleton<Camera>
 {
 private:
-	Player* player;
-	Vector2D location;
+	Player* player;		// プレイヤー情報
+	Vector2D location;	// カメラ座標
 
 public:
+	// コンストラクタ
 	Camera();
+	// デストラクタ
 	~Camera();
 
 public:
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
 	void Initialize();
+
+	/// <summary>
+	///  実行処理
+	/// </summary>
 	void Update();
+
+	/// <summary>
+	/// カメラ座標取得処理
+	/// </summary>
+	/// <returns>カメラ座標</returns>
 	Vector2D GetCameraPos();
+
+	/// <summary>
+	/// プレイヤー情報設定処理
+	/// </summary>
+	/// <param name="player">プレイヤー情報</param>
 	void SetPlayer(Player* player);
 };
 
